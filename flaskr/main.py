9965910d756005e8290c3ff848e5d7fc5dec9f75
Shirley Xu <xu33x@mtholyoke.edu>
2019-11-20 18:09:22 -0500

    fix path

--- flaskr/main.py
+++ flaskr/main.py
@@ -5,7 +5,7 @@ Setup:
     a. virtualenv env
     b. source env/bin/activate
     c. donwload relevant packages to create a project specific developing environment
-2. $ python main.py static/sample-outputs.json
+2. $ python main.py static/sample-output.json
 3. cmd+click the local host address.
 4. (optional) Hit shift+command+r (chrome) if css not loading new changes!(cache problem for chrome)
 """
@@ -33,12 +33,12 @@ def index():
         # do stuff when the form is submitted
         return redirect(url_for('update'))
 
-    return render_template('/stage1.html', history= history, 
+    return render_template('/stage1.html', history= history,
                                           item_inter = item_inter,
                                           cooc = cooc,
                                           simi = simi)
-    
-    
+
+
 @app.route('/update', methods= ['GET', 'POST'])
 def update():
     if request.method == 'POST':
@@ -46,10 +46,10 @@ def update():
     json_data, history, item_inter, cooc, simi = read_all(filename, timestamp=0)
 
     updated_hist, matrix_update, updated_item, updated_cooc, updated_simi = update_all(json_data, history, item_inter, cooc, simi, 1)
-    return render_template('/stage1_update.html', history= updated_hist, 
+    return render_template('/stage1_update.html', history= updated_hist,
                                           item_inter = updated_item,
                                           cooc = updated_cooc,
-                                          simi = updated_simi, 
+                                          simi = updated_simi,
                                           matrix_update= matrix_update)
 if __name__ == "__main__":
     app.run(debug = True)

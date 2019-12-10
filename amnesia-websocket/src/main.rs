7e1e2b49c87b8a69b2f4d6d2cf2208bb01568005
Shirley Xu <xu33x@mtholyoke.edu>
2019-12-10 18:38:47 -0500

    cleanup + merged websocket

---
+++ amnesia-websocket/src/main.rs
@@ -0,0 +1,112 @@
+extern crate timely;
+extern crate ws;
+extern crate amnesia_sigmod_demo;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use timely::communication::allocator::thread::Thread;
+use timely::worker::Worker;
+
+use differential_dataflow::input::InputSession;
+
+use ws::listen;
+
+use amnesia_sigmod_demo::server::Server;
+use differential_dataflow::operators::arrange::ArrangeByKey;
+use timely::dataflow::operators::Probe;
+use differential_dataflow::operators::join::JoinCore;
+use differential_dataflow::operators::{CountTotal,Count};
+
+fn main() {
+    let alloc = Thread::new();
+    let worker = Rc::new(RefCell::new(timely::worker::Worker::new(alloc)));
+
+    demo(Rc::clone(&worker));
+
+    while worker.borrow_mut().step_or_park(None) { }
+}
+
+fn demo(worker: Rc<RefCell<Worker<Thread>>>) {
+
+    let mut interactions_input: InputSession<usize, (u32, u32), isize> = InputSession::new();
+
+    let mut the_probe = timely::dataflow::operators::probe::Handle::new();
+
+    let (num_interactions_per_item_trace, cooccurrences_trace,
+        jaccard_similarities_trace)= worker.borrow_mut().dataflow(|scope| {
+
+        let interactions = interactions_input.to_collection(scope);
+
+        let num_interactions_per_item = interactions
+            .map(|(_user, item)| item)
+            .count_total();
+
+        let arranged_remaining_interactions = interactions.arrange_by_key();
+
+        // Compute the number of cooccurrences of each item pair
+        let cooccurrences = arranged_remaining_interactions
+            .join_core(&arranged_remaining_interactions, |_user, &item_a, &item_b| {
+                if item_a > item_b { Some((item_a, item_b)) } else { None }
+            })
+            .count();
+
+        let arranged_cooccurrences = cooccurrences.arrange_by_key();
+
+        let arranged_num_interactions_per_item = num_interactions_per_item.arrange_by_key();
+
+        // Compute the jaccard similarity between item pairs (= number of users that
+        // interacted with both items / number of users that interacted with at least
+        // one of the items)
+        let jaccard_similarities = cooccurrences
+            // Find the number of interactions for item_a
+            .map(|((item_a, item_b), num_cooc)| (item_a, (item_b, num_cooc)))
+            .join_core(
+                &arranged_num_interactions_per_item,
+                |&item_a, &(item_b, num_cooc), &occ_a| {
+                    Some((item_b, (item_a, num_cooc, occ_a)))
+                }
+            )
+            // Find the number of interactions for item_b
+            .join_core(
+                &arranged_num_interactions_per_item,
+                |&item_b, &(item_a, num_cooc, occ_a), &occ_b| {
+                    Some(((item_a, item_b), (num_cooc, occ_a, occ_b)))
+                },
+            )
+            // Compute Jaccard similarty, has to be done in a map due to the lack of a
+            // total order for f64 (which seems to break the consolidation in join)
+            .map(|((item_a, item_b), (num_cooc, occ_a, occ_b))| {
+                let jaccard = num_cooc as f64 / (occ_a + occ_b - num_cooc) as f64;
+                ((item_a, item_b), jaccard.to_string())
+            });
+
+        let arranged_jaccard_similarities = jaccard_similarities.arrange_by_key();
+
+        arranged_jaccard_similarities.stream.probe_with(&mut the_probe);
+
+        (arranged_num_interactions_per_item.trace, arranged_cooccurrences.trace,
+            arranged_jaccard_similarities.trace)
+    });
+
+    let input = Rc::new(RefCell::new(interactions_input));
+    let probe = Rc::new(RefCell::new(the_probe));
+    let shared_num_interactions_per_item_trace =
+        Rc::new(RefCell::new(num_interactions_per_item_trace));
+    let shared_cooccurrences_trace = Rc::new(RefCell::new(cooccurrences_trace));
+    let shared_similarities_trace = Rc::new(RefCell::new(jaccard_similarities_trace));
+
+    // Listen on an address and call the closure for each connection
+    listen("127.0.0.1:8000", |out| {
+        Server {
+            current_step: 0,
+            out,
+            worker: Rc::clone(&worker),
+            input: Rc::clone(&input),
+            probe: Rc::clone(&probe),
+            shared_num_interactions_per_item_trace: Rc::clone(&shared_num_interactions_per_item_trace),
+            shared_cooccurrences_trace: Rc::clone(&shared_cooccurrences_trace),
+            shared_similarities_trace: Rc::clone(&shared_similarities_trace)
+        }
+    }).unwrap();
+}
---
+++ temp/websocket/server.rs
@@ -0,0 +1,253 @@
+extern crate ws;
+extern crate serde_json;
+
+use timely::dataflow::ProbeHandle;
+
+use differential_dataflow::input::InputSession;
+use ws::{Handler, Message, Request, Response, Result, Sender};
+use differential_dataflow::operators::arrange::TraceAgent;
+use differential_dataflow::trace::implementations::spine_fueled::Spine;
+use differential_dataflow::trace::implementations::ord::OrdValBatch;
+use timely::communication::allocator::thread::Thread;
+use timely::worker::Worker;
+use differential_dataflow::trace::{Cursor, TraceReader};
+
+use std::fs::File;
+use std::io::Read;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::fmt::Debug;
+
+use serde_json::json;
+use serde_json::Result as SerdeResult;
+
+use crate::requests::{Change, ChangeRequest};
+use std::cmp::Ordering;
+
+type Trace<K, V> = TraceAgent<Spine<K, V, usize, isize, Rc<OrdValBatch<K, V, usize, isize>>>>;
+type SharedTrace<K, V> = Rc<RefCell<Trace<K, V>>>;
+
+pub struct Server {
+    pub current_step: usize,
+    pub out: Sender,
+    pub worker: Rc<RefCell<Worker<Thread>>>,
+    pub input: Rc<RefCell<InputSession<usize, (u32, u32), isize>>>,
+    pub probe: Rc<RefCell<ProbeHandle<usize>>>,
+    pub shared_num_interactions_per_item_trace: SharedTrace<u32, isize>,
+    pub shared_cooccurrences_trace: SharedTrace<(u32, u32), isize>,
+    pub shared_similarities_trace: SharedTrace<(u32, u32), String>,
+}
+
+fn read_local(file: &str) -> Vec<u8> {
+    let mut data = Vec::new();
+
+    let mut file = File::open(file).expect("Unable to read file!");
+    file.read_to_end(&mut data).expect("Unable to read file!");
+
+    data
+}
+
+#[derive(Eq, PartialEq)]
+struct ChangeMessage {
+    pub change: isize,
+    pub message: Message,
+}
+
+impl ChangeMessage {
+    pub fn new(change: isize, message: Message) -> Self {
+        ChangeMessage { change, message }
+    }
+}
+
+impl PartialOrd for ChangeMessage {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.change.cmp(&other.change))
+    }
+}
+
+impl Ord for ChangeMessage {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(&other).unwrap()
+    }
+}
+
+impl Server {
+
+    fn broadcast(&self, message: Message) {
+        self.out.broadcast(message).expect("Unable to send message");
+    }
+
+    fn last_update_time(&self) -> usize {
+        self.current_step - 1
+    }
+
+    fn broadcast_in_order(&self, mut changes: Vec<ChangeMessage>) {
+        changes.sort();
+        changes.into_iter()
+            .for_each(|change| self.broadcast(change.message));
+    }
+
+    fn broadcast_num_interactions_per_item_diffs(&self) {
+        let changes = collect_diffs(
+            Rc::clone(&self.shared_num_interactions_per_item_trace),
+            self.last_update_time(),
+            |item, count, time, change| {
+
+                let json = json!({
+                            "data": "item_interactions_n",
+                            "item": item,
+                            "count": count,
+                            "time": time,
+                            "change": change
+                        });
+
+                ChangeMessage::new(change, Message::text(json.to_string()))
+            });
+
+        self.broadcast_in_order(changes);
+    }
+
+    fn broadcast_cooccurrences_diffs(&self) {
+        let changes = collect_diffs(
+            Rc::clone(&self.shared_cooccurrences_trace),
+            self.last_update_time(),
+            |(item_a, item_b), num_cooccurrences, time, change| {
+
+                let json = json!({
+                            "data": "cooccurrences_c",
+                            "item_a": item_a,
+                            "item_b": item_b,
+                            "num_cooccurrences": num_cooccurrences,
+                            "time": time,
+                            "change": change
+                        });
+
+                ChangeMessage::new(change, Message::text(json.to_string()))
+            });
+
+        self.broadcast_in_order(changes);
+    }
+
+    fn broadcast_similarities_diffs(&self) {
+        let changes = collect_diffs(
+            Rc::clone(&self.shared_similarities_trace),
+            self.last_update_time(),
+            |(item_a, item_b), similarity, time, change| {
+
+                let json = json!({
+                            "data": "similarities_s",
+                            "item_a": item_a,
+                            "item_b": item_b,
+                            "similarity": similarity.parse::<f64>().unwrap(),
+                            "time": time,
+                            "change": change
+                        });
+
+                ChangeMessage::new(change, Message::text(json.to_string()))
+            });
+
+        self.broadcast_in_order(changes);
+    }
+}
+
+
+impl Handler for Server {
+    // Handle messages received in the websocket (in this case, only on /ws)
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+
+        // We assume we always get valid utf-8
+        let message_as_string = &msg.into_text().unwrap();
+
+        let parsed_request: SerdeResult<ChangeRequest> =
+            serde_json::from_slice(&message_as_string.as_bytes());
+
+        match parsed_request {
+            Ok(request) => {
+
+                println!("Received request: {:?}", request);
+
+                self.current_step += 1;
+
+                let mut interactions_input = self.input.borrow_mut();
+
+                if request.change == Change::Add {
+                    for (user, item) in request.interactions.iter() {
+                        interactions_input.insert((*user, *item));
+                    }
+                } else {
+                    for (user, item) in request.interactions.iter() {
+                        interactions_input.remove((*user, *item));
+                    }
+                }
+
+                interactions_input.advance_to(self.current_step);
+                interactions_input.flush();
+
+                self.worker.borrow_mut().step_while(|| {
+                    self.probe.borrow_mut().less_than(interactions_input.time())
+                });
+
+                println!("{:?}", request);
+
+                self.broadcast_num_interactions_per_item_diffs();
+                self.broadcast_cooccurrences_diffs();
+                self.broadcast_similarities_diffs();
+            },
+            Err(e) => println!("Error parsing request:\n{:?}\n\n{:?}\n", &message_as_string, e),
+        }
+
+        Ok(())
+    }
+
+    fn on_request(&mut self, req: &Request) -> Result<(Response)> {
+        match req.resource() {
+            "/ws" => Response::from_request(req),
+            "/style.css" => Ok(Response::new(200, "OK", read_local("html/style.css"))),
+            "/script.js" => Ok(Response::new(200, "OK", read_local("html/script.js"))),
+            "/images/Frozen.jpg" => Ok(Response::new(200, "OK", read_local("html/images/Frozen.jpg"))),
+            "/images/IT.jpg" => Ok(Response::new(200, "OK", read_local("html/images/IT.jpg"))),
+            "/images/Joker.jpg" => Ok(Response::new(200, "OK", read_local("html/images/Joker.jpg"))),
+            "/images/Midway.jpg" => Ok(Response::new(200, "OK", read_local("html/images/Midway.jpg"))),
+            "/" => Ok(Response::new(200, "OK", read_local("html/index.html"))),
+            _ => Ok(Response::new(404, "Not Found", b"404 - Not Found".to_vec())),
+        }
+    }
+}
+
+
+
+
+fn collect_diffs<K, V, F>(
+    trace: SharedTrace<K, V>,
+    time_of_interest: usize,
+    logic: F,
+) -> Vec<ChangeMessage>
+    where V: Clone + Ord + Debug,
+          K: Clone + Ord + Debug,
+          F: Fn(&K, &V, usize, isize) -> ChangeMessage + 'static
+{
+    // TODO don't like it that we have to buffer the messages here...
+    let mut messages = Vec::new();
+
+    let (mut cursor, storage) = trace.borrow_mut().cursor();
+
+    while cursor.key_valid(&storage) {
+        while cursor.val_valid(&storage) {
+
+            let key = cursor.key(&storage);
+            let value = cursor.val(&storage);
+
+            cursor.map_times(&storage, |time, diff| {
+                if *time == time_of_interest {
+                    messages.push(logic(&key, &value, *time, *diff));
+                }
+            });
+
+            cursor.step_val(&storage);
+        }
+        cursor.step_key(&storage);
+    }
+
+    messages
+}
